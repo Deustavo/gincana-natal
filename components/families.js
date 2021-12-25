f82a7f4eafd384887e23d5031a61f45edc58bba2
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-12-25 02:31:54 -0300

    tentando tratar o erro ai galera

--- components/families.js
+++ components/families.js
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import { useEasybase } from 'easybase-react';
 import { useEffect } from 'react/cjs/react.development';
+import { useState } from 'react';
 
 export default function Families({ setLoading }) {
+  const [hasError, setHasError] = useState(false);
   const { db, useReturn } = useEasybase();
   const { frame } = useReturn(() => db('GINCANANATAL'));
 
-  const handleScore = async (action, family) => {
+  const handleScore = (action, family) => {
     const index = frame.findIndex(n => n.name === family.name);
     const newScore = frame[index].score;
 
@@ -16,10 +18,11 @@ export default function Families({ setLoading }) {
       newScore = newScore - 1;
 
     setLoading(true);
-    await db('GINCANANATAL').where({ "name": family.name }).set({ score: newScore }).all();
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+    db('GINCANANATAL').where({ "name": family.name }).set({ score: newScore }).all().then(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }).catch(() => setHasError(true));
   };
 
   useEffect(() => {
@@ -27,8 +30,11 @@ export default function Families({ setLoading }) {
       setLoading(true);
     else
       setLoading(false);
-  }, [frame])
+  }, [frame]);
   
+  if (hasError)
+    return <p>Deu ruim</p>
+
   return<>
     <div className='container-families'>
       {frame.map((family, index) =>
