9f67c87e0e98dc45ac9785de6df523aa8f1c2b4f
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-12-25 02:23:10 -0300

    adicionando loading

--- components/families.js
+++ components/families.js
@@ -1,32 +1,37 @@
-import { useEffect } from 'react';
 import Image from 'next/image';
 import { useEasybase } from 'easybase-react';
+import { useEffect } from 'react/cjs/react.development';
 
-export default function Families() {
-  const { Frame, sync, configureFrame, db } = useEasybase();
-
-  useEffect(() => {
-    configureFrame({ tableName: "GINCANANATAL", limit: 10 });
-    sync();
-  }, []);
+export default function Families({ setLoading }) {
+  const { db, useReturn } = useEasybase();
+  const { frame } = useReturn(() => db('GINCANANATAL'));
 
   const handleScore = async (action, family) => {
-    const index = Frame().findIndex(n => n.name === family.name);
-    const newScore = Frame()[index].score;
+    const index = frame.findIndex(n => n.name === family.name);
+    const newScore = frame[index].score;
 
     if(action === 'add')
       newScore = newScore + 1;
     else
       newScore = newScore - 1;
 
-    console.log('salame');
+    setLoading(true);
     await db('GINCANANATAL').where({ "name": family.name }).set({ score: newScore }).all();
-    console.log('alface');
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   };
+
+  useEffect(() => {
+    if (frame.length === 0)
+      setLoading(true);
+    else
+      setLoading(false);
+  }, [frame])
   
   return<>
     <div className='container-families'>
-      {Frame().map((family, index) =>
+      {frame.map((family, index) =>
         <div key={index} className='family'>
           <Image className='family-image' src={family.image} alt={family.name} width={160} height={160}/>
           <div className='family-text'>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Families from '../components/families';
+import Loading from '../components/loading';
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
 
   return <>
     <section className='background-page'>
+      {loading && <Loading />}
       <h1>Gincana dos Andrade</h1>
-      <Families />
+      <Families setLoading={setLoading}/>
     </section>
   </>
 }
